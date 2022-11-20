05a02bbe13bc407513604766e863a19f2887b40a
Daniel Goren <danielgoren14@gmail.com>
2022-11-20 12:47:02 +0200

    initial commit

--- src/quadraticEquation.java
+++ src/quadraticEquation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exercise1 {
+public class quadraticEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int coefficient1;
