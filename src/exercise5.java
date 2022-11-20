bf0f491811287232d75b0ee28983fc7154f32478
Daniel Goren <danielgoren14@gmail.com>
2022-11-20 13:17:39 +0200

    initial commit

--- src/exercise5.java
+++ src/exercise5.java
@@ -8,25 +8,22 @@ public class exercise5 {
             System.out.println("Please enter a number");
             userInput = scanner.nextInt();
         } while (userInput < 0);
-        System.out.print("0 ");
         int number1 = 0;
         int number2 = 1;
         int number3 = 0;
         while(number3 < userInput){
-            System.out.print(number2 + " ");
             number3 = number1 + number2;
             number1 = number2;
             number2 = number3;
         }
-        System.out.print(number3);
         if(number3 == userInput)
         {
 
-            System.out.println(" the number "+ userInput +" belongs to fibonacci arithmetic progression");
+            System.out.println("the number "+ userInput +" belongs to fibonacci arithmetic progression");
         }
         else
         {
-            System.out.println(" the number "+ userInput + " doesn't belong to fibonacci arithmetic progression");
+            System.out.println("the number "+ userInput + " doesn't belong to fibonacci arithmetic progression");
         }
     }
 }
